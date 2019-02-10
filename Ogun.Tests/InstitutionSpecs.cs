03c774a7926f489b1f1daa2f844fada331b31f99
Shangoman <abdulbaruwa@hotmail.com>
2019-02-10 18:52:04 +0000

    More tests for UserAdded to customer event

--- Ogun.Tests/InstitutionSpecs.cs
+++ Ogun.Tests/InstitutionSpecs.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Ogun.GrainInterfaces.FourEyeModels;
+using Ogun.GrainInterfaces.FourEyeModels.Events;
 using Xunit;
 
 namespace Ogun.Tests
@@ -9,18 +10,40 @@ namespace Ogun.Tests
         [Fact]
         public void Handle_NewInstitutionEvent()
         {
-            var sut = new FourEyeInstitution();
-
-            var eventId = Guid.NewGuid();
-            var institutionId = Guid.NewGuid();
-            var testInstitutionName = "TestInstitution";
-            var @event = new NewInstitutionEvent<NewInstitution>(eventId, "TestEvent", DateTime.UtcNow, new NewInstitution(testInstitutionName, institutionId));
-
+            var institutionId = InitTestInstitution(out var sut, out var testInstitutionName, out var @event);
             sut.Causes(@event);
 
             Assert.Contains(@event, sut.Changes);
             Assert.Equal(sut.Name, testInstitutionName);
             Assert.Equal(sut.InstitutionId, institutionId);
+        }
+
+        [Fact]
+        public void Handle_UserAddedToInstitution()
+        {
+            var institutionId = InitTestInstitution(out var sut, out var testInstitutionName, out var @event);
+            sut.Causes(@event);
+
+            var userId = Guid.NewGuid();
+            var userAdded = new UserAddedToInstitution(userId);
+            var userAddedEvent = new UserAddedToInstitutionEvent<UserAddedToInstitution>(Guid.NewGuid(),
+                nameof(UserAddedToInstitution), DateTime.UtcNow, userAdded);
+
+            sut.Causes(userAddedEvent);
+
+            Assert.Contains(userAddedEvent, sut.Changes);
+            Assert.Contains(userId, sut.Approvers);
+        }
+
+        private static Guid InitTestInstitution(out FourEyeInstitution sut, out string testInstitutionName, out NewInstitutionEvent<NewInstitution> @event)
+        {
+            sut = new FourEyeInstitution();
+            var eventId = Guid.NewGuid();
+            var institutionId = Guid.NewGuid();
+            testInstitutionName = "TestInstitution";
+            @event = new NewInstitutionEvent<NewInstitution>(eventId, "TestEvent", DateTime.UtcNow,
+                new NewInstitution(testInstitutionName, institutionId));
+            return institutionId;
         }
     }
 }
